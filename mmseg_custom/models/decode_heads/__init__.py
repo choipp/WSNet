cda8e8e76a8864eca1b745f2c3e894ad340de4d5
arislid <arislid@gmail.com>
2023-02-13 08:12:21 +0000

    ADD: deeplabv3plus with ws

--- mmseg_custom/models/decode_heads/__init__.py
+++ mmseg_custom/models/decode_heads/__init__.py
@@ -2,4 +2,5 @@
 # Copyright (c) Shanghai AI Lab. All rights reserved.
 from .upernetws import UPerHeadWS
 from .fpn_headws import FPNHeadWS
-__all__ = ['UPerHeadWS', 'FPNHeadWS']
+from .sep_aspp_headws import DepthwiseSeparableASPPHeadWS
+__all__ = ['UPerHeadWS', 'FPNHeadWS', 'DepthwiseSeparableASPPHeadWS']
