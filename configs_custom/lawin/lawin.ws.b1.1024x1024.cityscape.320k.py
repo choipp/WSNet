a730b48fa2d67a9847d39ac9da91da3e192be7dd
revanZX <revanZXSD@gmail.com>
2023-02-23 08:55:27 +0000

    ADD : WS for lawin

---
+++ configs_custom/lawin/lawin.ws.b1.1024x1024.cityscape.320k.py
@@ -0,0 +1,46 @@
+_base_ = [
+    '../_base_/models/lawinws.py',
+    '../_base_/datasets/cityscapes_1024x1024_repeat.py',
+    '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_320k_adamw.py'
+]
+
+# model settings
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+find_unused_parameters = True
+model = dict(
+    type='EncoderDecoder',
+    backbone=dict(
+        type='mit_b1',
+        pretrained='/opt/ml/input/pretrained/mit_b1.pth',
+        style='pytorch'),
+    decode_head=dict(
+        type='LAWINHeadWS',
+        in_channels=[64, 128, 320, 512],
+        in_index=[0, 1, 2, 3],
+        channels=128,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        concat_fuse=True,
+        depth=1,
+        decoder_params=dict(embed_dim=128, in_dim=128, reduction=1, proj_type='conv', use_scale=True, mixing=False),
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    # model training and testing settings
+    train_cfg=dict(),
+    # test_cfg=dict(mode='whole'))
+    test_cfg=dict(mode='slide', crop_size=(1024,1024), stride=(768,768)))
+
+# optimizer
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
+                 paramwise_cfg=dict(custom_keys={'pos_block': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.),
+                                                 'head': dict(lr_mult=10.)
+                                                 }))
+
+lr_config = dict(_delete_=True, policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
