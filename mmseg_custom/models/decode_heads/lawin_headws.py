df8f709c88625d671d9b8b64c0e21acfe32e3c68
youngjun04 <daniel8370@naver.com>
2023-02-24 07:35:22 +0000

    fix:lawin ws

--- mmseg_custom/models/decode_heads/lawin_headws.py
+++ mmseg_custom/models/decode_heads/lawin_headws.py
@@ -3,5 +3,5 @@ from .ws_head import WS
 
 @HEADS.register_module()
 class LAWINHeadWS(WS):
-    def __init__(self, **kwargs):
+    def __init__(self, depth, concat_fuse, decoder_params, **kwargs):
         super(LAWINHeadWS, self).__init__(**kwargs)
