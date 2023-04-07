62c8f8dfd3b2e4112b0016852af5318311b58938
revanZX <revanZXSD@gmail.com>
2023-04-07 12:37:00 +0900

    Add : pspnet(WS) with coco-stuff10k.py

--- mmseg_custom/models/decode_heads/__init__.py
+++ mmseg_custom/models/decode_heads/__init__.py
@@ -7,5 +7,6 @@ from .ham_head import LightHamHead
 from .ham_headws import LightHamHeadWS
 from .lawin_head import LAWINHead
 from .lawin_headws import LAWINHeadWS
+from .psp_headws import PSPHeadWS
 __all__ = ['UPerHeadWS', 'FPNHeadWS', 'DepthwiseSeparableASPPHeadWS', 'LightHamHead', 'LightHamHeadWS',
-           'LAWINHead', 'LAWINHeadWS' ]
+           'LAWINHead', 'LAWINHeadWS', 'PSPHeadWS' ]
